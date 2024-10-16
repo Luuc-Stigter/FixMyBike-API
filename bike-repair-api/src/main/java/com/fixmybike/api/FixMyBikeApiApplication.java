d1f81766889d71bafbded687f6644318026c412b
Luuc Stigter <stigter.l@vepabins.nl>
2024-10-17 00:28:50 +0200

    project setup

---
+++ bike-repair-api/src/main/java/com/fixmybike/api/FixMyBikeApiApplication.java
@@ -0,0 +1,13 @@
+package com.fixmybike.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixMyBikeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixMyBikeApiApplication.class, args);
+	}
+
+}
---
+++ bike-repair-api/src/test/java/com/fixmybike/api/FixMyBikeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixmybike.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixMyBikeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
